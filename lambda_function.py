f4dc3b42488d35b6bde1124065c86bb129b37cd2
unknown <srivastavayashee1@gmail.com>
2024-09-01 00:11:54 +0530

    final Commit

--- lambda_function.py
+++ lambda_function.py
@@ -13,13 +13,13 @@ def lambda_handler(event,context):
     key = event['Records'][0]['s3']['object']['key']
     s3_client=boto3.client('s3')
     response=s3_client.get_object(Bucket=bucket,Key=key)
-    file_Content=response["Body"].read.decode('utf-8')
+    file_Content=response["Body"].read().decode('utf-8')
     lst=file_Content.split('\r\n')
     df=pd.DataFrame(columns=['id','status','amount','date'])
     for e in lst:
         dict=json.loads(e)
         if  dict['status']=="delivered":
-            df.loc[dict['status']]=dict
+            df.loc[dict['id']]=dict
     df.to_csv('/tmp/prcoesseddata.csv',sep=',')
     try:
         str_date=str(date.today())
@@ -30,8 +30,8 @@ def lambda_handler(event,context):
     upload_response=s3_client.upload_file('/tmp/prcoesseddata.csv',os.environ['targetbucket'],new_filename)
 
     sns_client=boto3.client('sns')
-    sns_client.publish(TopicArn=os.environ['snsarn']
-                       ,Message="file {} has been formated and Filter.Processed file is Placed in {} with Name {}". format(key,os.environ['targetbucket'],new_filename))
+    sns_client.publish(TopicArn=os.environ['snstopicarn']
+                       ,Message="file {} has been formated and Filter.Processed file is Placed in {} with Name {}".format(key,os.environ['targetbucket'],new_filename))
 
 
 
